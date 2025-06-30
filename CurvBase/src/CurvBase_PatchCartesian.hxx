4683d9486b4f549b3bb31422940a238b699eb049
Liwei Ji <jiliwei.phys@gmail.com>
2025-06-30 15:27:44 -0400

    CurvBase: add Cartesian patch

---
+++ CurvBase/src/CurvBase_PatchCartesian.hxx
@@ -0,0 +1,9 @@
+
+CCTK_HOST CCTK_DEVICE inline Coord cart_local_to_global(const Coord &l,
+                                                        const void *) {
+  return l;
+}
+CCTK_HOST CCTK_DEVICE inline Coord cart_global_to_local(const Coord &g,
+                                                        const void *) {
+  return g;
+}
