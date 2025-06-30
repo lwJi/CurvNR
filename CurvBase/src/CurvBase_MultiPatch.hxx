f04a1e32d5dce215bb6e5afea3d5bc270e2a1981
Liwei Ji <jiliwei.phys@gmail.com>
2025-06-30 14:50:30 -0400

    CurvBase: update class MultiPatch

--- CurvBase/src/CurvBase_MultiPatch.hxx
+++ CurvBase/src/CurvBase_MultiPatch.hxx
@@ -1,3 +1,16 @@
+
+struct PatchMap {
+  CCTK_HOST CCTK_DEVICE Coord (*local_to_global)(const Coord &,
+                                                 const void *) = nullptr;
+  CCTK_HOST CCTK_DEVICE Coord (*global_to_local)(const Coord &,
+                                                 const void *) = nullptr;
+};
+
+struct Patch {
+  PatchMap map;
+  void *meta = nullptr;
+};
+
 template <std::size_t N> class MultiPatch {
 public:
   CCTK_HOST CCTK_DEVICE MultiPatch() : count_(0) {} // empty by default
@@ -9,15 +22,14 @@ public:
   CCTK_HOST CCTK_DEVICE Coord local_to_global(std::size_t id,
                                               const Coord &l) const {
     return (id < count_)
-               ? patches_[id].funcs.local_to_global(l, patches_[id].userdata)
+               ? patches_[id].map.local_to_global(l, patches_[id].meta)
                : Coord{0, 0, 0};
   }
 
   CCTK_HOST CCTK_DEVICE Coord global_to_local(const Coord &g,
                                               std::size_t &id_out) const {
     for (std::size_t i = 0; i < count_; ++i) {
-      const Coord loc =
-          patches_[i].funcs.global_to_local(g, patches_[i].userdata);
+      const Coord loc = patches_[i].map.global_to_local(g, patches_[i].meta);
       if (is_valid_local(loc)) {
         id_out = i;
         return loc;
