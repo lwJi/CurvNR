f139f9e5275c9a0c620931debdb3a189ffee3884
Liwei Ji <jiliwei.phys@gmail.com>
2025-07-01 08:20:44 -0400

    CurvBase: add helpers that create individual Patch objects

--- CurvBase/src/CurvBase_Patch.hxx
+++ CurvBase/src/CurvBase_Patch.hxx
@@ -4,6 +4,10 @@
 #include <array>
 #include <cmath>
 
+#include "CurvBase_PatchCartesian.hxx"
+#include "CurvBase_PatchCubeSphere.hxx"
+#include "CurvBase_PatchSpherical.hxx"
+
 namespace CurvBase {
 
 using Coord = std::array<double, 3>;
@@ -20,6 +24,20 @@ struct Patch {
   PatchMap map;
   void *meta = nullptr;
 };
+
+// Helpers that create individual Patch objects
+inline Patch make_cart_patch() {
+  static CartesianMeta meta{};
+  return Patch{{cart_local_to_global, cart_global_to_local, cart_valid}, &meta};
+}
+inline Patch make_sph_patch(double r0, double r1) {
+  static SphericalMeta meta{r0, r1};
+  return Patch{{sph_local_to_global, sph_global_to_local, sph_valid}, &meta};
+}
+inline Patch make_wedge(Face f, double r0, double r1) {
+  static CubedSphereMeta meta{f, r0, r1};
+  return Patch{{cube_local_to_global, cube_global_to_local, cube_valid}, &meta};
+}
 
 } // namespace CurvBase
 
