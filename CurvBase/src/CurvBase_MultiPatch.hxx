7ad756f0921e53e4cc3d563e92632f95017bd1b8
Liwei Ji <jiliwei.phys@gmail.com>
2025-07-01 12:14:18 -0400

    CurvBase: fix typo in ActiveMultiPatch

--- CurvBase/src/CurvBase_MultiPatch.hxx
+++ CurvBase/src/CurvBase_MultiPatch.hxx
@@ -35,7 +35,7 @@ public:
 };
 
 struct ActiveMultiPatch {
-  PatchMode mode{PatchMode::Cartesian};
+  MultiPatchMode mode{MultiPatchMode::Cartesian};
 
   union {
     MultiPatch<1> mp1;
@@ -46,19 +46,19 @@ struct ActiveMultiPatch {
 
   // uniform API
   CCTK_HOST CCTK_DEVICE std::size_t size() const {
-    return (mode == PatchMode::CubeSphere) ? mp7.size() : mp1.size();
+    return (mode == MultiPatchMode::CubeSphere) ? mp7.size() : mp1.size();
   }
 
   CCTK_HOST CCTK_DEVICE Coord local_to_global(std::size_t id,
                                               const Coord &l) const {
-    return (mode == PatchMode::CubeSphere) ? mp7.local_to_global(id, l)
-                                           : mp1.local_to_global(id, l);
+    return (mode == MultiPatchMode::CubeSphere) ? mp7.local_to_global(id, l)
+                                                : mp1.local_to_global(id, l);
   }
 
   CCTK_HOST CCTK_DEVICE Coord global_to_local(const Coord &g,
                                               std::size_t &out) const {
-    return (mode == PatchMode::CubeSphere) ? mp7.global_to_local(g, out)
-                                           : mp1.global_to_local(g, out);
+    return (mode == MultiPatchMode::CubeSphere) ? mp7.global_to_local(g, out)
+                                                : mp1.global_to_local(g, out);
   }
 };
 
