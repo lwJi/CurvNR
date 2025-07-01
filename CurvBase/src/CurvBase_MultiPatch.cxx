7190caa0c5a71098c6ec1c71ad06417437e37953
Liwei Ji <jiliwei.phys@gmail.com>
2025-07-01 11:51:40 -0400

    CurvBase: add driver interface GetPatchSpecification and GetBoundarySpecification

--- CurvBase/src/CurvBase_MultiPatch.cxx
+++ CurvBase/src/CurvBase_MultiPatch.cxx
@@ -42,3 +42,37 @@ CurvBase_MultiPatch_GetSystemSpecification(CCTK_INT *restrict const npatches) {
   *npatches = mp->num_patches();
   return 0;
 }
+
+extern "C" CCTK_INT CurvBase_MultiPatch_GetPatchSpecification(
+    const CCTK_INT ipatch, CCTK_INT *restrict const is_cartesian,
+    const CCTK_INT size, CCTK_INT *restrict const ncells,
+    CCTK_REAL *restrict const xmin, CCTK_REAL *restrict const xmax) {
+  auto &mp = active_mp();
+  assert(ipatch >= 0 && ipatch < mp->num_patches());
+  assert(size == dim);
+  const Patch &patch = mp->patches.at(ipatch);
+
+  if (is_cartesian != nullptr) {
+    *is_cartesian = static_cast<CCTK_INT>(patch.is_cartesian);
+  }
+
+  for (int d = 0; d < dim; ++d) {
+    ncells[d] = patch.ncells[d];
+    xmin[d] = patch.xmin[d];
+    xmax[d] = patch.xmax[d];
+  }
+  return 0;
+}
+
+extern "C" CCTK_INT CurvBase_MultiPatch_GetBoundarySpecification(
+    const CCTK_INT ipatch, const CCTK_INT size,
+    CCTK_INT *restrict const is_interpatch_boundary) {
+  auto &mp = active_mp();
+  assert(ipatch >= 0 && ipatch < mp->num_patches());
+  assert(size == 2 * dim);
+  const Patch &patch = mp->patches.at(ipatch);
+  for (int f = 0; f < 2; ++f)
+    for (int d = 0; d < dim; ++d)
+      is_interpatch_boundary[2 * d + f] = !patch.faces[f][d].is_outer_boundary;
+  return 0;
+}
