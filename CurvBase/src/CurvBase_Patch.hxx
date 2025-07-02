e0764ee4caaac1b403af540beaf37e6662ec3e37
Liwei Ji <jiliwei.phys@gmail.com>
2025-07-01 22:47:21 -0400

    CurvBase: add ncells, xmin, xmax, faces to Patch

--- CurvBase/src/CurvBase_Patch.hxx
+++ CurvBase/src/CurvBase_Patch.hxx
@@ -10,7 +10,7 @@
 
 namespace CurvBase {
 
-using Coord = std::array<double, 3>;
+using Coord = std::array<CCTK_REAL, 3>;
 
 struct PatchMap {
   CCTK_HOST CCTK_DEVICE Coord (*local_to_global)(const Coord &,
@@ -20,9 +20,19 @@ struct PatchMap {
   CCTK_HOST CCTK_DEVICE bool (*is_valid_local)(const Coord &) = nullptr;
 };
 
+struct FaceInfo {
+  bool is_outer_boundary{true};
+};
+
 struct Patch {
-  PatchMap map;
-  void *meta = nullptr;
+  PatchMap map{};
+  void *meta{nullptr};
+
+  std::array<CCTK_INT, dim> ncells{}; // cells per dimension
+  std::array<CCTK_REAL, dim> xmin{};  // lower physical bounds
+  std::array<CCTK_REAL, dim> xmax{};  // upper physical bounds
+
+  std::array<std::array<FaceInfo, dim>, 2> faces{};
 };
 
 // Helpers that create individual Patch objects
@@ -30,11 +40,11 @@ inline Patch make_cart_patch() {
   static CartesianMeta meta{};
   return Patch{{cart_local_to_global, cart_global_to_local, cart_valid}, &meta};
 }
-inline Patch make_sph_patch(double r0, double r1) {
+inline Patch make_sph_patch(CCTK_REAL r0, CCTK_REAL r1) {
   static SphericalMeta meta{r0, r1};
   return Patch{{sph_local_to_global, sph_global_to_local, sph_valid}, &meta};
 }
-inline Patch make_wedge(Face f, double r0, double r1) {
+inline Patch make_wedge(Face f, CCTK_REAL r0, CCTK_REAL r1) {
   static CubedSphereMeta meta{f, r0, r1};
   return Patch{{cubedsphere_local_to_global, cubedsphere_global_to_local,
                 cubedsphere_valid},
