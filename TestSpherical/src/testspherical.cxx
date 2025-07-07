5bb83c44317c0e60c89cc47e7a0d4db71adb33d3
Liwei Ji <jiliwei.phys@gmail.com>
2025-07-06 22:32:17 -0400

    TestSpherical: add src

---
+++ TestSpherical/src/testspherical.cxx
@@ -0,0 +1,49 @@
+#include <loop_device.hxx>
+
+#include <cctk.h>
+#include <cctk_Arguments.h>
+#include <cctk_Parameters.h>
+
+#include <array>
+#include <cassert>
+#include <cmath>
+
+namespace TestSpherical {
+using namespace Loop;
+
+// u(t,r) = (f(t-r) - f(t+r)) / r
+// f(v) = A exp(-1/2 (r/W)^2)
+template <typename T>
+constexpr void gaussian(const T A, const T W, const T t, const T x, const T y,
+                        const T z, T &u, T &rho) {
+  using std::exp, std::pow, std::sqrt;
+
+  const T r = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+  const auto f = [&](const T v) {
+    return A * exp(-pow(v, 2) / (2 * pow(W, 2)));
+  };
+
+  if (r < sqrt(std::numeric_limits<T>::epsilon())) {
+    // L'Hôpital
+    u = 2 / pow(W, 2) * f(t) * t;
+    rho = -2 / pow(W, 4) * f(t) * (pow(t, 2) - pow(W, 2));
+  } else {
+    u = (f(t - r) - f(t + r)) / r;
+    rho = -(f(t - r) * (t - r) - f(t + r) * (t + r)) / (pow(W, 2) * r);
+  }
+}
+
+extern "C" void TestSpherical_Initial(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_ARGUMENTSX_TestSpherical_Initial;
+  DECLARE_CCTK_PARAMETERS;
+
+  grid.loop_int_device<1, 1, 1>(
+      grid.nghostzones,
+      [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+        CCTK_REAL rho;
+        gaussian(amplitude, gaussian_width, cctk_time, p.x, p.y, p.z, u(p.I),
+                 rho);
+      });
+}
+
+} // namespace TestSpherical
