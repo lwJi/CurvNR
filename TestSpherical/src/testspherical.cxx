a3a6a245c280751647216a366aff6ba1791fa5f5
Liwei Ji <jiliwei.phys@gmail.com>
2025-07-09 15:01:59 -0400

    TestSpherical: add rhs calculation

--- TestSpherical/src/testspherical.cxx
+++ TestSpherical/src/testspherical.cxx
@@ -43,7 +43,7 @@ constexpr void sinewave(const T A, const T freq, const T t, const T x,
   const T omega = 2.0 * M_PI * freq;
 
   u = A * sin(2.0 * M_PI * dot - omega * t);
-  rho = 0.0; // TODO: fix this
+  rho = -A * omega * cos(2.0 * M_PI * dot - omega * t);
 }
 
 extern "C" void TestSpherical_Initial(CCTK_ARGUMENTS) {
@@ -54,27 +54,57 @@ extern "C" void TestSpherical_Initial(CCTK_ARGUMENTS) {
     grid.loop_int_device<1, 1, 1>(
         grid.nghostzones,
         [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
-          const CCTK_REAL t = 0.0;
           const CCTK_REAL kx = 0.1;
           const CCTK_REAL ky = 0.1;
           const CCTK_REAL kz = 0.1;
           const CCTK_REAL freq = 1.0;
-          CCTK_REAL rho;
-          sinewave(amplitude, freq, t, ccoordx(p.I), ccoordy(p.I), ccoordz(p.I),
-                   kx, ky, kz, u(p.I), rho);
+          sinewave(amplitude, freq, cctk_time, ccoordx(p.I), ccoordy(p.I),
+                   ccoordz(p.I), kx, ky, kz, u(p.I), rho(p.I));
         });
   } else if (CCTK_EQUALS(initial_condition, "Gaussian")) {
     grid.loop_int_device<1, 1, 1>(
         grid.nghostzones,
         [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
-          CCTK_REAL t = 6.8;
-          CCTK_REAL rho;
-          gaussian(amplitude, gaussian_width, t, ccoordx(p.I), ccoordy(p.I),
-                   ccoordz(p.I), u(p.I), rho);
+          gaussian(amplitude, gaussian_width, cctk_time, ccoordx(p.I),
+                   ccoordy(p.I), ccoordz(p.I), u(p.I), rho(p.I));
         });
   } else {
     CCTK_ERROR("Unknown initial condition");
   }
+}
+
+extern "C" void TestSpherical_RHS(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_ARGUMENTSX_TestSpherical_RHS;
+  DECLARE_CCTK_PARAMETERS;
+
+  grid.loop_int_device<1, 1, 1>(
+      grid.nghostzones,
+      [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+        CCTK_REAL r = p.x;
+        CCTK_REAL r2 = r * r;
+        CCTK_REAL sinth = std::sin(p.y);
+        CCTK_REAL costh = std::cos(p.y);
+        CCTK_REAL cotth = costh / sinth;
+        CCTK_REAL sinth2 = sinth * sinth;
+
+        std::array<CCTK_REAL, dim> du, ddu;
+        for (int d = 0; d < dim; ++d) {
+          du[d] = ((u(p.I - 2 * p.DI[d]) - u(p.I + 2 * p.DI[d])) -
+                   8 * (u(p.I - p.DI[d]) - u(p.I + p.DI[d]))) /
+                  (12 * p.DX[d]);
+          ddu[d] = (-(u(p.I - 2 * p.DI[d]) + u(p.I + 2 * p.DI[d])) +
+                    16 * (u(p.I - p.DI[d]) + u(p.I + p.DI[d])) - 30 * u(p.I)) /
+                   (12 * pow(p.DX[d], 2));
+        }
+
+        u_rhs(p.I) = rho(p.I);
+        rho_rhs(p.I) = ddu[0] + 2 * du[0] / r + (cotth * du[1] + ddu[1]) / r2 +
+                       ddu[2] / (r2 * sinth2);
+      });
+}
+
+extern "C" void TestSpherical_Sync(CCTK_ARGUMENTS) {
+  // do nothing
 }
 
 } // namespace TestSpherical
