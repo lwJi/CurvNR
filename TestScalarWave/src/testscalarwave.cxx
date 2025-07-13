3e7b6c49d40c62536d6c733f758b5f8bfb29e2a6
Liwei Ji <jiliwei.phys@gmail.com>
2025-07-13 18:33:26 -0400

    TestScalarWave: include cylindrical

--- TestScalarWave/src/testscalarwave.cxx
+++ TestScalarWave/src/testscalarwave.cxx
@@ -86,8 +86,6 @@ extern "C" void TestScalarWave_RHS(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTS_TestScalarWave_RHS;
   DECLARE_CCTK_PARAMETERS;
 
-  constexpr int deriv_order = 4;
-
   for (int d = 0; d < 3; ++d)
     if (cctk_nghostzones[d] < deriv_order / 2)
       CCTK_VERROR("Need at least %d ghost zones", deriv_order / 2);
@@ -105,144 +103,113 @@ extern "C" void TestScalarWave_RHS(CCTK_ARGUMENTS) {
       cdJ1xx, cdJ1xy, cdJ1xz, cdJ1yy, cdJ1yz, cdJ1zz, cdJ2xx, cdJ2xy, cdJ2xz,
       cdJ2yy, cdJ2yz, cdJ2zz, cdJ3xx, cdJ3xy, cdJ3xz, cdJ3yy, cdJ3yz, cdJ3zz};
 
+  // allocate temporary GF3D5 gfs
+  const int ntmps = 9;
+  int itmp = 0;
+  CXUtils::GF3D5Factory<CCTK_REAL> fct(layout5, ntmps, itmp);
+  const auto tl_du = fct.make_vec_gf();
+  const auto tl_ddu = fct.make_smat_gf();
+  if (itmp != ntmps)
+    CCTK_VERROR("Wrong number of temporary variables: ntmps=%d itmp=%d", ntmps,
+                itmp);
+  itmp = -1;
+
   const Loop::GridDescBaseDevice grid(cctkGH);
 
-  if (combine_derivs_jacobian) {
+  if (use_jacobian) {
 
-    // allocate temporary GF3D5 gfs
-    const int ntmps = 9;
-    int itmp = 0;
-    CXUtils::GF3D5Factory<CCTK_REAL> fct(layout5, ntmps, itmp);
-    const auto tl_duCart = fct.make_vec_gf();
-    const auto tl_dduCart = fct.make_smat_gf();
-    if (itmp != ntmps)
-      CCTK_VERROR("Wrong number of temporary variables: ntmps=%d itmp=%d",
-                  ntmps, itmp);
-    itmp = -1;
-
+    // Derivatives of Cartesian Coordinate
     switch (deriv_order) {
     case 2: {
-      CurvDerivs::calc_transderivs<1, 1, 1, 2>(grid, layout5, tl_duCart,
-                                               tl_dduCart, layout2, u, invDxyz,
-                                               gf_Jac, gf_dJac);
+      CurvDerivs::calc_transderivs<1, 1, 1, 2>(
+          grid, layout5, tl_du, tl_ddu, layout2, u, invDxyz, gf_Jac, gf_dJac);
       break;
     }
     case 4: {
-      CurvDerivs::calc_transderivs<1, 1, 1, 4>(grid, layout5, tl_duCart,
-                                               tl_dduCart, layout2, u, invDxyz,
-                                               gf_Jac, gf_dJac);
+      CurvDerivs::calc_transderivs<1, 1, 1, 4>(
+          grid, layout5, tl_du, tl_ddu, layout2, u, invDxyz, gf_Jac, gf_dJac);
+      break;
+    }
+    case 6: {
+      CurvDerivs::calc_transderivs<1, 1, 1, 6>(
+          grid, layout5, tl_du, tl_ddu, layout2, u, invDxyz, gf_Jac, gf_dJac);
+      break;
+    }
+    case 8: {
+      CurvDerivs::calc_transderivs<1, 1, 1, 8>(
+          grid, layout5, tl_du, tl_ddu, layout2, u, invDxyz, gf_Jac, gf_dJac);
       break;
     }
     default:
       assert(0 && "Invalid derivative order");
     }
 
-    const auto dduCart11 = tl_dduCart[0].ptr;
-    const auto dduCart22 = tl_dduCart[3].ptr;
-    const auto dduCart33 = tl_dduCart[5].ptr;
+    const auto ddu11 = tl_ddu[0].ptr;
+    const auto ddu22 = tl_ddu[3].ptr;
+    const auto ddu33 = tl_ddu[5].ptr;
 
-    grid.loop_int_device<
-        1, 1, 1>(grid.nghostzones, [=] CCTK_DEVICE(
-                                       const Loop::PointDesc
-                                           &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
-      const int ijk = layout2.linear(p.i, p.j, p.k);
-      const int ijk5 = layout5.linear(p.i, p.j, p.k);
-
-      u_rhs[ijk] = rho[ijk];
-      rho_rhs[ijk] = dduCart11[ijk5] + dduCart22[ijk5] + dduCart33[ijk5];
-
-      if (std::isnan(rho_rhs[ijk])) {
-        printf("iter = %i, xyz = %16.8e, %16.8e, %16.8e,   ijk = %i, %i, %i,\n",
-               cctk_iteration, p.x, p.y, p.z, p.i, p.j, p.k);
-        assert(0);
-      }
-    });
+    grid.loop_int_device<1, 1, 1>(
+        grid.nghostzones,
+        [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+          const int ijk = layout2.linear(p.i, p.j, p.k);
+          const int ijk5 = layout5.linear(p.i, p.j, p.k);
+          u_rhs[ijk] = rho[ijk];
+          rho_rhs[ijk] = ddu11[ijk5] + ddu22[ijk5] + ddu33[ijk5];
+        });
 
   } else {
 
-    // allocate temporary GF3D5 gfs
-    const int ntmps = 18;
-    int itmp = 0;
-    CXUtils::GF3D5Factory<CCTK_REAL> fct(layout5, ntmps, itmp);
-    const auto tl_duSph = fct.make_vec_gf();
-    const auto tl_dduSph = fct.make_smat_gf();
-    const auto tl_duCart = fct.make_vec_gf();
-    const auto tl_dduCart = fct.make_smat_gf();
-    if (itmp != ntmps)
-      CCTK_VERROR("Wrong number of temporary variables: ntmps=%d itmp=%d",
-                  ntmps, itmp);
-    itmp = -1;
+    // Derivatives of Curvilinear Coordinate
+    CXUtils::calc_derivs2nd<1, 1, 1>(grid, layout5, tl_du, tl_ddu, layout2, u,
+                                     invDxyz, deriv_order);
 
-    // Derivatives of Spherical Coordinate
-    CXUtils::calc_derivs2nd<1, 1, 1>(grid, layout5, tl_duSph, tl_dduSph,
-                                     layout2, u, invDxyz, deriv_order);
+    const auto du1 = tl_du[0].ptr;
+    const auto du2 = tl_du[1].ptr;
+    const auto du3 = tl_du[2].ptr;
+    const auto ddu11 = tl_ddu[0].ptr;
+    const auto ddu22 = tl_ddu[3].ptr;
+    const auto ddu33 = tl_ddu[5].ptr;
 
-    if (use_jacobian) {
+    if (CCTK_EQUALS(patch_system, "Spherical")) {
+      grid.loop_int_device<1, 1, 1>(
+          grid.nghostzones,
+          [=] CCTK_DEVICE(const Loop::PointDesc &p)
+              CCTK_ATTRIBUTE_ALWAYS_INLINE {
+                const int ijk = layout2.linear(p.i, p.j, p.k);
+                const int ijk5 = layout5.linear(p.i, p.j, p.k);
+                const auto r = p.x;
+                const auto r2 = r * r;
+                const auto sinth = std::sin(p.y);
+                const auto costh = std::cos(p.y);
+                const auto cotth = costh / sinth;
+                const auto sinth2 = sinth * sinth;
 
-      // Transformation from Spherical to Cartesian Coordinate
-      CurvDerivs::calc_trans<1, 1, 1>(grid, layout5, tl_duCart, tl_dduCart,
-                                      tl_duSph, tl_dduSph, layout2, gf_Jac,
-                                      gf_dJac);
+                u_rhs[ijk] = rho[ijk];
+                rho_rhs[ijk] = ddu11[ijk5] + 2 * du1[ijk5] / r +
+                               (cotth * du2[ijk5] + ddu22[ijk5]) / r2 +
+                               ddu33[ijk5] / (r2 * sinth2);
+              });
+    } else if (CCTK_EQUALS(patch_system, "Cylindrical")) {
+      grid.loop_int_device<1, 1, 1>(
+          grid.nghostzones,
+          [=] CCTK_DEVICE(const Loop::PointDesc &p)
+              CCTK_ATTRIBUTE_ALWAYS_INLINE {
+                const int ijk = layout2.linear(p.i, p.j, p.k);
+                const int ijk5 = layout5.linear(p.i, p.j, p.k);
+                const auto rh = p.x;
+                const auto rh2 = rh * rh;
+                const auto sinph = std::sin(p.y);
+                const auto cosph = std::cos(p.y);
 
-      const auto dduCart11 = tl_dduCart[0].ptr;
-      const auto dduCart22 = tl_dduCart[3].ptr;
-      const auto dduCart33 = tl_dduCart[5].ptr;
-
-      grid.loop_int_device<
-          1, 1, 1>(grid.nghostzones, [=] CCTK_DEVICE(
-                                         const Loop::PointDesc
-                                             &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
-        const int ijk = layout2.linear(p.i, p.j, p.k);
-        const int ijk5 = layout5.linear(p.i, p.j, p.k);
-
-        u_rhs[ijk] = rho[ijk];
-        rho_rhs[ijk] = dduCart11[ijk5] + dduCart22[ijk5] + dduCart33[ijk5];
-
-        if (std::isnan(rho_rhs[ijk])) {
-          printf(
-              "iter = %i, xyz = %16.8e, %16.8e, %16.8e,   ijk = %i, %i, %i,\n",
-              cctk_iteration, p.x, p.y, p.z, p.i, p.j, p.k);
-          assert(0);
-        }
-      });
-
+                u_rhs[ijk] = rho[ijk];
+                rho_rhs[ijk] = ddu11[ijk5] + du1[ijk5] / rh +
+                               ddu22[ijk5] / rh2 + ddu33[ijk5];
+              });
     } else {
-
-      const auto du1 = tl_duSph[0].ptr;
-      const auto du2 = tl_duSph[1].ptr;
-      const auto du3 = tl_duSph[2].ptr;
-      const auto ddu11 = tl_dduSph[0].ptr;
-      const auto ddu22 = tl_dduSph[3].ptr;
-      const auto ddu33 = tl_dduSph[5].ptr;
-
-      grid.loop_int_device<
-          1, 1, 1>(grid.nghostzones, [=] CCTK_DEVICE(
-                                         const Loop::PointDesc
-                                             &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
-        const int ijk = layout2.linear(p.i, p.j, p.k);
-        const int ijk5 = layout5.linear(p.i, p.j, p.k);
-
-        const auto r = p.x;
-        const auto r2 = r * r;
-        const auto sinth = std::sin(p.y);
-        const auto costh = std::cos(p.y);
-        const auto cotth = costh / sinth;
-        const auto sinth2 = sinth * sinth;
-
-        u_rhs[ijk] = rho[ijk];
-        rho_rhs[ijk] = ddu11[ijk5] + 2 * du1[ijk5] / r +
-                       (cotth * du2[ijk5] + ddu22[ijk5]) / r2 +
-                       ddu33[ijk5] / (r2 * sinth2);
-
-        if (std::isnan(rho_rhs[ijk])) {
-          printf(
-              "iter = %i, xyz = %16.8e, %16.8e, %16.8e,   ijk = %i, %i, %i,\n",
-              cctk_iteration, p.x, p.y, p.z, p.i, p.j, p.k);
-          assert(0);
-        }
-      });
+      CCTK_ERROR("Unknown patch system");
     }
   }
+}
 }
 
 extern "C" void TestScalarWave_Sync(CCTK_ARGUMENTS) {
