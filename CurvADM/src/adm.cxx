8612d634c626b8af7664e441d6dc2c825b7ec194
Liwei Ji <jiliwei.phys@gmail.com>
2025-07-28 08:53:08 -0400

    CurvADM: add thorn

---
+++ CurvADM/src/adm.cxx
@@ -0,0 +1,80 @@
+#include <loop_device.hxx>
+
+#include <cctk.h>
+#include <cctk_Arguments.h>
+#include <cctk_Parameters.h>
+
+#include <array>
+
+namespace CurvADM {
+using namespace std;
+using namespace Loop;
+
+extern "C" void CurvADM_initial_data(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_ARGUMENTSX_CurvADM_initial_data;
+  DECLARE_CCTK_PARAMETERS;
+
+  grid.loop_all_device<1, 1, 1>(grid.nghostzones,
+                                [=] CCTK_DEVICE(const PointDesc &p)
+                                    CCTK_ATTRIBUTE_ALWAYS_INLINE {
+                                      gxx(p.I) = 1;
+                                      gxy(p.I) = 0;
+                                      gxz(p.I) = 0;
+                                      gyy(p.I) = 1;
+                                      gyz(p.I) = 0;
+                                      gzz(p.I) = 1;
+
+                                      kxx(p.I) = 0;
+                                      kxy(p.I) = 0;
+                                      kxz(p.I) = 0;
+                                      kyy(p.I) = 0;
+                                      kyz(p.I) = 0;
+                                      kzz(p.I) = 0;
+                                    });
+}
+
+extern "C" void CurvADM_initial_lapse(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_ARGUMENTSX_CurvADM_initial_lapse;
+  DECLARE_CCTK_PARAMETERS;
+
+  grid.loop_all_device<1, 1, 1>(
+      grid.nghostzones, [=] CCTK_DEVICE(const PointDesc &p)
+                            CCTK_ATTRIBUTE_ALWAYS_INLINE { alp(p.I) = 1; });
+}
+
+extern "C" void CurvADM_initial_dtlapse(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_ARGUMENTSX_CurvADM_initial_dtlapse;
+  DECLARE_CCTK_PARAMETERS;
+
+  grid.loop_all_device<1, 1, 1>(
+      grid.nghostzones, [=] CCTK_DEVICE(const PointDesc &p)
+                            CCTK_ATTRIBUTE_ALWAYS_INLINE { dtalp(p.I) = 0; });
+}
+
+extern "C" void CurvADM_initial_shift(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_ARGUMENTSX_CurvADM_initial_shift;
+  DECLARE_CCTK_PARAMETERS;
+
+  grid.loop_all_device<1, 1, 1>(grid.nghostzones,
+                                [=] CCTK_DEVICE(const PointDesc &p)
+                                    CCTK_ATTRIBUTE_ALWAYS_INLINE {
+                                      betax(p.I) = 0;
+                                      betay(p.I) = 0;
+                                      betaz(p.I) = 0;
+                                    });
+}
+
+extern "C" void CurvADM_initial_dtshift(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_ARGUMENTSX_CurvADM_initial_dtshift;
+  DECLARE_CCTK_PARAMETERS;
+
+  grid.loop_all_device<1, 1, 1>(grid.nghostzones,
+                                [=] CCTK_DEVICE(const PointDesc &p)
+                                    CCTK_ATTRIBUTE_ALWAYS_INLINE {
+                                      dtbetax(p.I) = 0;
+                                      dtbetay(p.I) = 0;
+                                      dtbetaz(p.I) = 0;
+                                    });
+}
+
+} // namespace CurvADM
